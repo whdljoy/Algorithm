c176a8586f2c4e8466adf096e3d2e998efddc947
HeoJongWon <hgw4846@gmail.com>
2023-08-02 22:41:21 +0900

    change directory

--- Java/practice/p1018.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-import java.io.*;
-public class p1018 {
-    public static void main(String[] args) {
-
-    }
-}
--- samsung/14501.py
+++
@@ -1,3 +0,0 @@
-n = int (input())
-val = list(list(map(int, input().split())) for _ in range(n))
-print(val)
