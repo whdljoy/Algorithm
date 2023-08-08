3ffd1d5ee45cc74f649aaeb3f7bca87760b0238e
Jongwon <hgw4846@gmail.com>
2023-08-08 19:06:02 +0900

    today's solve

--- Java/Main_16935_배열돌리기.java
+++ Java/Main_16935_배열돌리기.java
@@ -14,7 +14,7 @@ public class Main_16935_배열돌리기 {
 	static int []cal;
 	static int [][] origin;
 	static int [][] ans;
-	static int arr_max;
+
 	public static void main(String[] args)  throws Exception{
 		// TODO Auto-generated method stub
 		run();
@@ -25,21 +25,27 @@ public class Main_16935_배열돌리기 {
 			switch (cal[i]) {
 			case 1:
 				first();
+				copy_arr();
 				break;
 			case 2:
 				second();
+				copy_arr();
 				break;
 			case 3:
 				third();
+				copy_arr();
 				break;
 			case 4:
 				fourth();
+				copy_arr();
 				break;	
 			case 5:
 				fifth();
+				copy_arr();
 				break;
 			case 6:
 				sixth();
+				copy_arr();
 				break;				
 			}
 		}
@@ -50,9 +56,8 @@ public class Main_16935_배열돌리기 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
-		arr_max = Math.max(N, M);
-		origin = new int[arr_max+1][arr_max+1];
-		ans = new int[arr_max+1][arr_max+1];
+		origin = new int[N+1][M+1];
+		ans = new int[N+1][M+1];
 		for(int y=1; y<=N; y++) {
 			st =new StringTokenizer(br.readLine());
 			for(int x=1; x<=M;x++) {
@@ -65,7 +70,21 @@ public class Main_16935_배열돌리기 {
 			cal[i] = Integer.parseInt(st.nextToken());
 		}
 	}
-	
+
+	public static void swap(){
+		int tmp =N;
+		N =M;
+		M =tmp;
+	}
+
+	public static void copy_arr(){
+		origin = new int[N+1][M+1];
+		for(int y=1; y<=N;y++){
+			for(int x=1;x<=M; x++){
+				origin[y][x] =ans[y][x];
+			}
+		}
+	}
 	public static void first() {
 		for(int x =1;x<=M;x++) {
 			for(int y=1;y<=N; y++) {
@@ -83,55 +102,59 @@ public class Main_16935_배열돌리기 {
 	}
 	
 	public static void third() {
-		for(int y=1; y<=arr_max; y++) {
-			for(int x=1;x<=arr_max; x++) {
-				ans[x][arr_max+1-y] =origin[y][x];
+		ans = new int[M+1][N+1];
+		for(int y=1; y<=N; y++) {
+			for(int x=1;x<=M; x++) {
+				ans[x][N+1-y] =origin[y][x];
 			}
 		}
+		swap();
 	}
 	public static void fourth() {
-		for(int y=1; y<=arr_max; y++) {
-			for(int x=1;x<=arr_max; x++) {
-				ans[arr_max+1-x][y] =origin[y][x];
+		ans = new int[M+1][N+1];
+		for(int y=1; y<=N; y++) {
+			for(int x=1;x<=M; x++) {
+				ans[M+1-x][y] =origin[y][x];
 			}
 		}
+		swap();
 	}	
 	public static void fifth() {
 		int fd_x = M/2;
 		int fd_y = N/2;
+		ans = new int[N+1][M+1];
 		for(int y=1;y<=fd_y; y++) {  //1번을 2번으로
 			for(int x=1;x<=fd_x; x++) {
 				ans[y][fd_x+x]= origin[y][x];
 			}
 		}
-		for(int y=fd_y;y<=arr_max; y++) { //4번을 1번으로
+		for(int y=fd_y;y<=N; y++) { //4번을 1번으로
 			for(int x=1;x<=fd_x; x++) {
 				ans[y-fd_y][x]= origin[y][x];
 			}
 		}
-		for(int x=fd_x;x<=M; x++) { //2번을 3번으로
+		for(int x=fd_x+1;x<=M; x++) { //2번을 3번으로
 			for(int y=1;y<=fd_y; y++) {
 				ans[y+fd_y][x]= origin[y][x];
 			}
-		}		
-		for(int x=fd_x;x<=M; x++) { //3번을 4번으로
-			for(int y=fd_y;y<=N; y++) {
+		}
+		for(int x=fd_x+1;x<=M; x++) { //3번을 4번으로
+			for(int y=fd_y+1;y<=N; y++) {
 				ans[y][x-fd_x]= origin[y][x];
 			}
-		}			
+		}
 		
 	}
 	public static void sixth() {
 		int fd_x = M/2;
 		int fd_y = N/2;
-		
-		
+		ans = new int[N+1][M+1];
 		for(int y=1;y<=fd_y; y++) {  //1번을 4번으로
 			for(int x=1; x<=fd_x; x++) {
 				ans[y+fd_y][x]= origin[y][x];
 			}
 		}
-		for(int y=fd_y;y<=N; y++) { //4번을 3번으로
+		for(int y=fd_y+1;y<=N; y++) { //4번을 3번으로
 			for(int x=1;x<=fd_x; x++) {
 				ans[y][x+fd_x]= origin[y][x];
 			}
@@ -140,28 +163,19 @@ public class Main_16935_배열돌리기 {
 			for(int y=1;y<=fd_y; y++) {
 				ans[y][x-fd_x]= origin[y][x];
 			}
-		}			
-		for(int x=fd_x; x<=M; x++) { //3번을 2번으로
-			for(int y=fd_y; y<=N; y++) {
+		}
+		for(int x=fd_x+1; x<=M; x++) { //3번을 2번으로
+			for(int y=fd_y+1; y<=N; y++) {
 				ans[y-fd_y][x]= origin[y][x];
 			}
-		}	
+		}
 	}
 	public static void print_ans() throws Exception{
-		StringBuilder sbb =new StringBuilder();
-		for(int y=1;y<=arr_max;y++) {
-			for(int x=1;x<=arr_max; x++) {
-				sbb.append(origin[y][x]).append(" ");
-			}
-			sbb.append("\n");
-		}
-		bw.write(sbb.toString());
 		StringBuilder sb =new StringBuilder();
-		for(int y =1;y<=arr_max;y++) {
-			for(int x=1;x<=arr_max; x++) {
-				//if(ans[y][x]!=0) {
-					sb.append(ans[y][x]).append(" ");
-				//}
+		for(int y =1;y<=N;y++) {
+			for(int x=1;x<=M; x++) {
+				sb.append(ans[y][x]).append(" ");
+
 			}
 			sb.append("\n");
 		}
