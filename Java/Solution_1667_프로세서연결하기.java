7d16381ccb67d4f9155e716bd85ba19d0c587240
HeoJongWon <37110949+HeoJongWon@users.noreply.github.com>
2023-08-23 00:02:42 +0900

    Update today's solve

--- Java/Solution_1667_프로세서연결하기.java
+++ Java/Solution_1667_프로세서연결하기.java
@@ -7,6 +7,8 @@ public class Solution_1667_프로세서연결하기 {
     static StringBuilder sb = new StringBuilder();
     static int N;
     static int[][] origin;
+    static int MAX_CORE;
+    static boolean flag;
     static List <Integer[]> core;
     static boolean [] check;
     static int ans =Integer.MAX_VALUE;
@@ -21,22 +23,29 @@ public class Solution_1667_프로세서연결하기 {
         for(int i=1;i<=tc;i++){
             sb.append("#").append(i).append(" ");
             input();
-            for(int c_idx=1;c_idx<=core.size();c_idx++){
-                select_core(0,-1, c_idx);
+            for(int c_idx=1;c_idx<=MAX_CORE;c_idx++){
+                select_core(0,0, c_idx);
             }
             sb.append(ans).append("\n");
         }
         System.out.println(sb);
     }
-    static boolean check_size(int y, int x){  //side 에 있으면 true
+    static boolean check_size(int y, int x){
+        int wall=0;
         for(int dir=0;dir<4;dir++){
             int cy=y + dy[dir];
-            int cx = x+ dx[dir];
+            int cx =x + dx[dir];
+            if(origin[cy][cx] ==1){
+                wall+=1;
+            }
             if(!in_range(cy,cx)){
                 return true;
             }
         }
-        return false;  //없으면 false
+        if(wall ==4){
+            return true;
+        }
+        return false;
     }
     static boolean in_range(int y,int x){
         return 0<=x && x<N && 0<=y && y<N;
@@ -56,8 +65,8 @@ public class Solution_1667_프로세서연결하기 {
                 }
             }
         }
-
-        check = new boolean[core.size()];
+        MAX_CORE = core.size();
+        check = new boolean[MAX_CORE];
         ans =Integer.MAX_VALUE;
         core_ans =Integer.MIN_VALUE;
    }
@@ -66,15 +75,15 @@ public class Solution_1667_프로세서연결하기 {
             cal_line();
             return;
         }
-        for(int i =idx+1; i<core.size();i++){
+        for(int i =idx; i<MAX_CORE;i++){
             check[i]= true;
-            select_core(num+1,i,target);
+            select_core(num+1,i+1,target);
             check[i]= false;
         }
     }
     static void cal_line(){
         select = new ArrayList<>();
-        for(int i=0; i< core.size();i++){
+        for(int i=0; i< MAX_CORE;i++){
             if(check[i]){
                 select.add(core.get(i));
             }
@@ -87,10 +96,14 @@ public class Solution_1667_프로세서연결하기 {
             return;
         }
         for(int dir=0;dir<4; dir++) {
-            search(idx+1,change_status(select.get(idx),dir,status));
+            int [][] cp =change_status(idx,dir,status);
+            if(flag){
+                search(idx+1,cp);
+            }
         }
     }
-    static int [][] change_status(Integer[] core_info, int direction,int [][] cp){
+    static int [][] change_status(int idx, int direction,int [][] cp){
+        Integer [] core_info = select.get(idx);
         int [][] status = new int[N][N];
         for(int y=0;y<N;y++){
             for(int x=0; x<N; x++){
@@ -99,7 +112,7 @@ public class Solution_1667_프로세서연결하기 {
         }
         int cx = core_info[1];
         int cy = core_info[0];
-        boolean flag =true;
+        flag =true;
         while(true){
             cx+=dx[direction];
             cy+=dy[direction];
@@ -110,21 +123,8 @@ public class Solution_1667_프로세서연결하기 {
                 flag =false;
                 break;
             }
+            status[cy][cx] =2;
         }
-        if(flag){
-            int change_x = core_info[1];
-            int change_y = core_info[0];
-            while(true){
-                change_x+=dx[direction];
-                change_y+=dy[direction];
-                if(!in_range(change_y,change_x)){
-                    break;
-                }
-                status[change_y][change_x] =2;
-            }
-        }
-
-
 
         return status;
     }
