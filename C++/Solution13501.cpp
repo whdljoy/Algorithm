2ee8e146d72005246533f150675f1aa750466760
Jongwon <hgw4846@gmail.com>
2023-09-20 13:20:35 +0900

    update

--- C++/Solution13501.cpp
+++ C++/Solution13501.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 
@@ -8,16 +9,51 @@ using namespace std;
 #define NL "\n"
 stringstream sb;
 
-int tc;
+int tc,N,M,L;
+vector <int> origin;
 
+
+void cal(){
+  for (int i = 0; i < M; i++){
+    char ch;
+    cin >> ch;
+    if(ch == 'I'){
+      int val, position;
+      cin >> val >> position;
+      auto it = origin.begin();
+      advance(it, position);
+      origin.insert(it, val);
+    }
+    else if (ch == 'D')
+    {
+      int position;
+      cin >> position;
+      auto it = origin.begin();
+      advance(it, position);
+      origin.erase(it);
+      origin.shrink_to_fit();
+    }
+    else if (ch == 'C')
+    {
+      int p1, p2;
+      cin >> p1 >> p2;
+    }
+  }
+}
 
 void input(){
-  
+  cin >> N >> M >> L;
+  for (int i = 0; i < N;i++){
+    int cur;
+    cin >> cur;
+    origin.push_back(cur);
+  }
 }
 void run(){
   cin >> tc;
   for (int i = 1; i <= tc; i++){
     input();
+    cal();
     sb << "#" << i << " ";
   }
 }
