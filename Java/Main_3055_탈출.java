b90e7b762e99ca0cd5143c5a80d0a79d98c6543f
HeoJongWon <hgw4846@gmail.com>
2023-10-03 23:52:09 +0900

    update

--- Java/Main_3055_탈출.java
+++ Java/Main_3055_탈출.java
@@ -1,129 +1,94 @@
 
+
 import java.util.*;
 
 import java.awt.Point;
 import java.io.*;
 public class Main_3055_탈출 {
-	static final int MAX_N =51;
-	static final int MAX_TIME =3000;
-	static class Move{
-		Point position;
-		int time=0;
-		Move(Point position, int time){
-			this.position = position;
-			this.time = time;
-		}
-	}
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
 	static int R,C;
-	static int origin[][];
-	static int water[][][];
-	static Point start;
+	static char origin[][];
+	static Deque <Point> water;
+	static Deque <Integer []> move;
+	
 	static int dx[] = {0 ,0,1,-1};
 	static int dy[] = {-1,1,0,0};
 	static int ans =Integer.MAX_VALUE;
 	static boolean flag = false;
+	static boolean visited[][];
+	
  	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
  		input();
- 		increase_water();
  		bfs();
  		
  		if(ans ==Integer.MAX_VALUE) {
- 			sb.append("KAKTUS");
+ 			System.out.println("KAKTUS");
 
  		}else {
- 			sb.append(ans);
+ 			System.out.println(ans);
  		}
- 		System.out.println(sb);
 	}
 	static void input() throws Exception{
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		origin = new int[ MAX_N][ MAX_N];
-		water = new int[MAX_TIME][MAX_N][MAX_N];
+		origin = new char[ R][C];
+		water = new ArrayDeque<>();
+		move = new ArrayDeque<>();
+		visited= new boolean[R][C];
 		for(int y=0; y<R; y++) {
 			String ip = br.readLine();
 			for(int x=0; x<C; x++) {
 				char cur = ip.charAt(x);
+				origin[y][x] = cur;
 				if(cur == 'S') {
-					start = new Point(x,y);
-					origin[y][x] =-1;
-				}else if (cur == 'D') origin[y][x] = 2;
-				else if (cur == '*') origin[y][x] = 1;
-				else if (cur == 'X') origin[y][x] = 3;
-				else origin[y][x] = 0;
+					move.push(new Integer[] {x,y,0});
+					visited[y][x] = true;
+				}
+				else if (cur == '*') {
+					water.add(new Point(x,y));
+				}
 			}
 		}
 	}
 	
 	static void bfs() {
-		boolean visited[][] = new boolean [MAX_N][MAX_N];
-		Deque <Move> dq = new ArrayDeque<>();
-		dq.push(new Move(start, 0));
-		visited[start.y][start.x] = true;
-		while(!dq.isEmpty() && !flag) {
-			Move cur = dq.pollFirst();
-			int [][] tmp = water[cur.time+1];
-			print(tmp);
-			for(int dir=0;dir<4;dir++) {
-				int cy = cur.position.y +dy[dir];
-				int cx= cur.position.x +dx[dir];
-				if(in_range(cy, cx) && !visited[cy][cx]) {
-					if( tmp[cy][cx] == 0 ) {
-						dq.push(new Move(new Point(cx,cy),cur.time+1));
-						visited[cy][cx]= true;
-					}
-					if(tmp[cy][cx] ==2) {
-						ans = cur.time+1;
-						flag =true;
+		while(!move.isEmpty()) {
+			int cur_water = water.size();
+			for(int i=0;i<cur_water;i++) {
+				Point water_p = water.pollFirst();
+				for(int dir=0;dir<4;dir++) {
+					int cy = water_p.y +dy[dir];
+					int cx = water_p.x +dx[dir];
+					if(in_range(cy, cx) && origin[cy][cx]== '.') {
+						water.add(new Point(cx,cy));
+						origin[cy][cx] = '*';
 					}
 				}
 			}
-			
-		}
-	}
-	static void print(int water [][]) {
-		for(int y=0;y<R;y++) {
-			for(int x=0;x<C;x++) {
-				System.out.print(water[y][x]);
-			}
-			System.out.println();
-		}
-	}
-	static void increase_water() {	
-		water[0]=cp_arr(origin);
-		for(int i=1;i<=MAX_TIME;i++) {
-			int [][] cp = cp_arr(water[i-1]);
-			for(int y=0;y<R;y++) {
-				for(int x=0; x<C; x++) {
-					if(water[i-1][y][x]==1) {
-						for(int dir=0; dir<4; dir++) {
-							int cx = x+dx[dir];
-							int cy = y+dy[dir];
-							if(in_range(cy, cx) && water[i-1][cy][cx] == 0) {
-								cp[cy][cx]= 1;
-							}
+			int cur_move = move.size();
+			for(int i=0;i<cur_move;i++) {
+				Integer[] move_p = move.pollFirst();
+				for(int dir=0;dir<4;dir++) {
+					int cy = move_p[1] +dy[dir];
+					int cx = move_p[0] +dx[dir];
+					int time = move_p[2];
+					if(in_range(cy, cx) &&!visited[cy][cx]) {
+						if(origin[cy][cx] == 'D') {
+							ans =time+1;
+							return;
 						}
+						else if(origin[cy][cx]=='.') {
+							visited[cy][cx] = true;
+							move.add(new Integer[] {cx,cy,time+1});
+						}						
 					}
-				}
-			
+				}				
 			}
-			water[i]=cp_arr(cp);
 		}
-		
+
 	}
-	static int [][] cp_arr (int [][] arr) {
-		int cp[][] =new int[MAX_N][MAX_N];	
-		for(int y=0;y<R;y++) {
-			for(int x=0; x<C; x++) {
-				cp[y][x] = arr[y][x];
-			}
-		}	
-		return cp;
-	}
+
 	static boolean in_range(int y,int x) {
 		return 0<=y && y< R && 0<=x && x<C;
 	}
