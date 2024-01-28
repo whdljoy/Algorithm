93c2720d0b295b152f3926a67654e20f3979ce1b
HeoJongWon <37110949+HeoJongWon@users.noreply.github.com>
2024-01-29 00:16:24 +0900

    Today's update

---
+++ Samsung/Solution_K번째문자열.java
@@ -0,0 +1,93 @@
+import java.util.*;
+import java.io.*;
+
+
+/**
+ *
+ *
+ *  LCP  로도 풀수있음
+ */
+public class Solution_K번째문자열 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int TC=10;
+    static int K;
+    static String word;
+
+
+    static class Node {
+        HashMap <Character, Node> childNode = new HashMap<>();
+        boolean isLast;
+
+        Map <Character, Node> getChild(){
+            return this.childNode;
+        }
+    }
+
+    static class Trie{
+        Node rootNode;
+
+        Trie(){
+            rootNode = new Node();
+        }
+
+        void insert(String word){
+            Node thisNode = this.rootNode;
+            for(int i=word.length()-1; i>-1;i--){
+                thisNode =thisNode.getChild().computeIfAbsent(word.charAt(i),c-> new Node());
+            }
+
+        }
+
+        String [] contains(String word){
+            Node thisNode = this.rootNode;
+            String [] save = new String[word.length()];
+            String tmp = "";
+            for(int i=word.length()-1;i>-1;i--) {
+                char cur = word.charAt(i);
+                Node node = thisNode.getChild().get(cur);
+                if (node == null) return save;
+                tmp = cur+tmp;
+                save[i] = tmp;
+                thisNode = node;
+            }
+            return save;
+        }
+    }
+    public static void main(String[] args) throws  Exception{
+        run();
+    }
+    static  void run() throws Exception {
+        TC = Integer.parseInt(br.readLine());
+        for(int i=1;i<=TC;i++){
+            sb.append("#").append(i).append(" ");
+            input();
+        }
+        System.out.println(sb);
+    }
+    static void input() throws Exception{
+        K = Integer.parseInt(br.readLine());
+        word = br.readLine();
+        String [] answer = new String[word.length()];
+        Trie current = new Trie();
+        current.insert(word);
+        answer = current.contains(word);
+        HashSet<String> total =new HashSet<>();
+        for(int i=0; i< answer.length;i++){
+            String cur = "";
+            for(int j=0;j<answer[i].length();j++){
+                cur += answer[i].charAt(j);
+                total.add(cur);
+            }
+        }
+        answer = (String[]) total.toArray(new String[0]);
+        Arrays.sort(answer);
+        if(K >= total.size()){
+            sb.append("none").append("\n");
+        }else{
+            sb.append(answer[K-1]).append("\n");
+        }
+
+    }
+}
--- Samsung/Solution_K번째접미사.java
+++ Samsung/Solution_K번째접미사.java
@@ -7,10 +7,15 @@ public class Solution_K번째접미사 {
     static int TC =10;
     static int K;
     static String word;
+
+    static Trie current;
     static class Node {
         Map <Character, Node> child = new HashMap<>();
+
+
         boolean last;
         Map <Character, Node> getChild(){
+
             return this.child;
         }
         void setLast (boolean last){
@@ -25,21 +30,50 @@ public class Solution_K번째접미사 {
         }
         void insert(String wrd){
             Node thisNode = this.rootNode;
-            for(int i=0;i <wrd.length();i++){
+            for(int i=wrd.length()-1;i >-1;i--){
                 thisNode = thisNode.getChild().computeIfAbsent(word.charAt(i),c-> new Node());
             }
         }
-        boolean contains(String wrd){
+        String [] contains(String wrd){
             Node thisNode = this.rootNode;
-            for(int i=0; i< wrd.length();i++){
+            String [] ans = new String[wrd.length()];
+            String tmp = "";
+            for(int i=wrd.length()-1; i>-1;i--){
                 char cur = word.charAt(i);
+                tmp = cur +tmp;
+                ans [i]=tmp;
                 Node node = thisNode.getChild().get(cur);
-                if(node == null) return false;
-
+                if(node == null) return ans;
                 thisNode = node;
 
+
             }
-            return thisNode.last;
+
+            return ans;
+        }
+        void delete(String word){
+            delete(this.rootNode,word,0);
+        }
+        private void delete(Node thisNode ,String word, int index){
+            char character = word.charAt(index);
+            if(!thisNode.getChild().containsKey(character)) return;
+
+            Node childNode = thisNode.getChild().get(character);
+            index++;
+            if(index == word.length()){
+                if(!childNode.last) return;
+
+               childNode.setLast(false);
+
+                if(childNode.getChild().isEmpty()) {
+                    thisNode.getChild().remove(character);
+                }
+            }else{
+                delete(childNode,word,index);
+                if(!childNode.last && childNode.getChild().isEmpty()){
+                    thisNode.getChild().remove(character);
+                }
+            }
         }
     }
     public static void main(String[] args) throws Exception{
@@ -50,12 +84,18 @@ public class Solution_K번째접미사 {
         for(int i=1;i<=TC;i++) {
             sb.append("#").append(i).append(" ");
             input();
+
         }
         System.out.print(sb);
     }
     static void input() throws Exception{
         K = Integer.parseInt(br.readLine());
         word = br.readLine();
+        current = new Trie();
+        current.insert(word);
+        String [] answer = current.contains(word);
+        Arrays.sort(answer);
 
+        sb.append(answer[K-1]).append("\n");
     }
 }
